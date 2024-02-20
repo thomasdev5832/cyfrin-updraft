2541e8de3a9eb00cee81de8c449b492812546dc1
thomasdev5832 <eugabrielthome@gmail.com>
2024-02-19 23:18:29 -0300

    9. deploy a mock priceFeed

---
+++ foundry-fundamentals/foundry-fund-me/script/HelperConfig.s.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+// 1, deploy mocks when we are on a local anvil chain
+// 2. keep track of contract address across different chains
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig {
+    // if we are on a local anvil, we deploy mocks
+    // otherwise, grab the existing address from the live network
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return ethConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
+}
