9c899ea5f595488fc2d0e611055acf010fe1a6e4
thomasdev5832 <eugabrielthome@gmail.com>
2024-02-21 23:16:25 -0300

    foundry tests cheatcodes

--- foundry-fundamentals/foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-fundamentals/foundry-fund-me/test/FundMeTest.t.sol
@@ -16,10 +16,15 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
 
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public {
@@ -34,12 +39,18 @@ contract FundMeTest is Test {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
     }
-}
 
-/*
-4 types of testing:
-1. Unit: Testing a single function
-2. Integration: Testing multiple functions
-3. Forked: Testing on a forked network
-4. Staging: Testing on a live network (testnet or mainnet)
-*/
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // hey, the next line should revert!!!
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.startPrank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        vm.stopPrank();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+}
