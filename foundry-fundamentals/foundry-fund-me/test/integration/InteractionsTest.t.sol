8c461b0fc34a3b25eaf36e385c7a8c5b411912af
thomasdev5832 <eugabrielthome@gmail.com>
2024-03-05 14:00:13 -0300

    integration tests

---
+++ foundry-fundamentals/foundry-fund-me/test/integration/InteractionsTest.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+
+contract InteractionsTest is Test {
+    FundMe fundMe;
+
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 public constant STARTING_USER_BALANCE = 10 ether;
+
+    function setUp() external {
+        DeployFundMe deploy = new DeployFundMe();
+        fundMe = deploy.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testUserCanFundInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        fundFundMe.fundFundMe(address(fundMe));
+
+        WithdrawFundMe withdrawFundme = new WithdrawFundMe();
+        withdrawFundme.withdrawFundMe(address(fundMe));
+
+        assert(address(fundMe).balance == 0);
+    }
+}
