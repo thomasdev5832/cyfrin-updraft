95d4a532eb632b772f8ed36a456dd2f874867550
thomasdev5832 <eugabrielthome@gmail.com>
2024-03-04 22:55:39 -0300

    storage and gas costs optimization

--- foundry-fundamentals/foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-fundamentals/foundry-fund-me/test/FundMeTest.t.sol
@@ -129,4 +129,39 @@ contract FundMeTest is Test {
                 fundMe.getOwner().balance - startingOwnerBalance
         );
     }
+
+    function testWithdrawFromMultipleFundersCheaper() public funded {
+        // arrange
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 2;
+        for (
+            uint160 i = startingFunderIndex;
+            i < numberOfFunders + startingFunderIndex;
+            i++
+        ) {
+            // we get hoax from stdcheats
+            // prank + deal
+            hoax(address(i), STARTING_USER_BALANCE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+
+        // act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
+        vm.stopPrank();
+
+        // assert
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+        assert(
+            (numberOfFunders + 1) * SEND_VALUE ==
+                fundMe.getOwner().balance - startingOwnerBalance
+        );
+    }
 }
